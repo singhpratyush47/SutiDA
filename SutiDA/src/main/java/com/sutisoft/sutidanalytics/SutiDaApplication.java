6afe9b1b2a9c21422b50741de60540f68000a290
pratyushs <pratyushs@ibi190.infobrain.com>
2019-07-19 19:44:56 +0530

    initial commit
    initial commit

---
+++ SutiDA/src/main/java/com/sutisoft/sutidanalytics/SutiDaApplication.java
@@ -0,0 +1,13 @@
+package com.sutisoft.sutidanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SutiDaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SutiDaApplication.class, args);
+	}
+
+}
